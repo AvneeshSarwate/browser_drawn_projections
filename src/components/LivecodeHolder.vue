b4132b8819253a7853650741c5792fb36db1826b
Avneesh Sarwate <avneeshsarwate@gmail.com>
2023-09-24 15:26:55 +0200

    adding try-catch to prevent hot-reload from crashing

--- src/components/LivecodeHolder.vue
+++ src/components/LivecodeHolder.vue
@@ -7,18 +7,23 @@ import { inject, onMounted, onUnmounted } from 'vue';
 const appState = inject('appState') as AppState  
 
 const reg = (i: number) => appState.regions.list[i]
+const cornerPts = (reg: Region, p5: p5) => {
+  reg.points.list.forEach(p => {
+    p5.circle(p.x, p.y, 10)
+  })
+}
 
 
 onMounted(() => {
-  if (appState.p5Instance && appState.regions.list.length > 0 ) {
-    // reg(0).draw2 = (reg: Region, p5: p5) => {
-    //   reg.points.list.forEach(p => {
-    //     p5.circle(p.x, p.y, 10)
-    //   })
-    // }
-    reg(0).draw2 = undefined
+  try {
+    if (appState.p5Instance && appState.regions.list.length > 0) {
+      reg(0).draw2 = cornerPts
+      // reg(0).draw2 = undefined
+    }
+    const z = appState.regions.list.map(r => r.draw)
+  } catch (e) {
+    console.log(e)
   }
-  const z = appState.regions.list.map(r => r.draw)
 
 })
 
