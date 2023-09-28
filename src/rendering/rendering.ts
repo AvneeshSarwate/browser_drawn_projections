7cd2ff4145569263bebfb8e5458cba66ae8a6a30
Avneesh Sarwate <avneeshsarwate@gmail.com>
2023-09-28 09:37:23 -0400

    first draft at implementing shaderNode type effect

--- src/rendering/rendering.ts
+++ src/rendering/rendering.ts
@@ -1,5 +1,6 @@
 import { findClosestPointAndRegion, type AppState, Region } from '@/stores/stores'
 import p5 from 'p5'
+import * as THREE from 'three'
 
 export function createP5Sketch(canvas: HTMLCanvasElement, appState: () => AppState): p5 {
   const sketch = (p: p5) => {
@@ -89,4 +90,92 @@ export function createP5Sketch(canvas: HTMLCanvasElement, appState: () => AppSta
   }
 
   return new p5(sketch, canvas)
+}
+
+
+const errorImageTexture = new THREE.TextureLoader().load('error.png')
+const defaultRenderTarget = new THREE.WebGLRenderTarget(1280, 720)
+const renderer = new THREE.WebGLRenderer({canvas: document.getElementById('threeCanvas') as HTMLCanvasElement})
+
+abstract class ShaderEffect {
+  setSrc(fx: (ShaderEffect|HTMLCanvasElement)[]): void {}
+  render(): void {}
+  output: THREE.WebGLRenderTarget = defaultRenderTarget
+  width: number = 1280
+  height: number = 720
+}
+
+//used with the "glsl-literal" vscode plugin to get syntax highlighting for embedded glsl
+const glsl = (x: any): string => x
+
+const planeVS = glsl`
+precision highp float;
+
+attribute vec3 position;
+attribute vec2 uv;
+
+uniform mat4 worldViewProjection;
+
+varying vec2 vUV;
+
+void main() {
+  vec4 p = vec4(position, 1.);
+  gl_Position = worldViewProjection * p;
+  vUV = uv;
+}`
+
+const passThruFS = glsl`
+precision highp float;
+
+varying vec2 vUV;
+
+uniform sampler2D tex;
+
+void main() {
+  gl_FragColor = texture2D(tex, vUV);
+}`
+
+class Passthru implements ShaderEffect {
+  output: THREE.WebGLRenderTarget
+  width: number
+  height: number
+  scene: THREE.Scene
+  camera: THREE.Camera
+  inputs: (ShaderEffect | HTMLCanvasElement)[] = []
+  material: THREE.ShaderMaterial
+  constructor(width = 1280, height = 720) {
+    this.output = new THREE.WebGLRenderTarget(width, height)
+    this.width = width
+    this.height = height
+    //a scene with an orthographic camera, a single plane, and a shader material
+    this.scene = new THREE.Scene()
+    this.camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1)
+    const geometry = new THREE.PlaneGeometry(2, 2)
+    this.material = new THREE.ShaderMaterial({
+      vertexShader: planeVS,
+      fragmentShader: passThruFS,
+      uniforms: {
+        tex: { value: errorImageTexture }
+      }
+    })
+    const mesh = new THREE.Mesh(geometry, this.material)
+    this.scene.add(mesh)
+  }
+
+  setShader(fragmentString: string): void {
+    this.material.fragmentShader = fragmentString
+    this.material.needsUpdate = true
+  }
+
+  setSrc(fx: (ShaderEffect | HTMLCanvasElement)[]): void {
+    this.inputs = fx
+    const input = fx[0] ? fx[0] : errorImageTexture
+    const inputVal = input instanceof ShaderEffect ? (input as ShaderEffect).output.texture : input
+    this.material.uniforms.tex.value = inputVal
+  }
+  render(): void {
+    //render to the output
+    renderer.setRenderTarget(this.output)
+    renderer.render(this.scene, this.camera)
+  }
 }
