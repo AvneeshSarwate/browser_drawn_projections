0d9d3eb2144a1f3d753465d3eb062024c44f694d
Avneesh Sarwate <avneeshsarwate@gmail.com>
2023-09-29 11:57:12 -0400

    basic example of branch/wait with slightly longer than wanted syntax

--- src/channels/channels.ts
+++ src/channels/channels.ts
@@ -1,5 +1,26 @@
 
 
-const f = () => {
-  
+//define an async function that waits using setTimeout
+async function wait(ms: number) {
+  return new Promise<void>((resolve, reject) => {
+      setTimeout(() => {
+          resolve();
+      }, ms);
+  });
 }
+
+function branch<T>(block: () => Promise<T>): Promise<T> {
+    return block();
+}
+
+const res = branch(async () => {
+  console.log('start')
+  wait(10)
+  console.log('end')
+})
+
+res.then(() => {
+    console.log('done')
+})
+
+
