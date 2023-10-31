4a78b8b8bbc34491e2533b92fa72ac532c193c74
Avneesh Sarwate <avneeshsarwate@gmail.com>
2023-10-31 13:09:21 -0400

    more three5style design sketching

--- src/rendering/three5Style.ts
+++ src/rendering/three5Style.ts
@@ -6,7 +6,7 @@ import { Three5 } from './three5';
 //used with the "glsl-literal" vscode plugin to get syntax highlighting for embedded glsl
 const glsl = (x: any): string => x[0]
 
-
+//todo api - make this specific to three5style allowed values
 type StaticShaderUniforms = {
   [key: string]: ShaderUniform
 }
@@ -46,33 +46,65 @@ class Three5Style {
   
   public material: THREE.Material
   public uniforms: StaticShaderUniforms = {}
+  public mainString = ""
 
-  constructor(mainString: string, uniforms: StaticShaderUniforms) {
-    this.uniforms = uniforms
+  constructor() {
+
     this.material = new THREE.ShaderMaterial(
       {
         vertexShader: planeVS,
-        fragmentShader: this.baseFragmentShader.replace("Replace String", mainString), //todo incomplete
+        fragmentShader: this.baseFragmentShader.replace("Replace String", this.mainString), //todo incomplete
         uniforms: {}
       }
     )
   }
 
-  public clone() { //call this "getMaterial()"
-    //push the uniform values from the uniform object into the actual material uniforms, and return the material
-    //do you need to dispose of these materials? maintain a cache based on shader string?
-    //should this be embedded inside three5, or be a utility that hooks in/works on top of it? 
-      //(probs want to expose setting arbitrary custom material anyway)
+  public clone(): THREE.Material { //call this "getMaterial()"
+    /*
+    push the uniform values from the uniform object into the actual material uniforms, and return the material
+    do you need to dispose of these materials? maintain a cache based on shader string?
+    should this be embedded inside three5, or be a utility that hooks in/works on top of it? 
+      - (probs want to expose setting arbitrary custom material anyway)
+      - give three5 a "setStyle" that takes either a raw material or three5Style and internally uses/disposes accordingly
+
+    
+    
+    in three5, have a useMaterial or useStyle method that switches whether the "style mode" uses a raw three material or a three5Style
+    - if using a raw material, user is responsible for handling update of the material uniforms as dicated by three
+    - if using a three5style, user can set type-safe properties on the three5style and then three5 will extract an 
+      appropriately cloned/managed material when creating a mesh
+    */
+      return this.material
+  }
+}
+
+//todo api - is there a way to map user-custom names to the [type][0-3] names ergonomically? by order of key definition?
+//is there a way to properly replace symbols in the shader? eg first float detected replaces USER_FLOAT_0 and up to 
+//the user not to pick names that break string-replacement?
+class LineStyle extends Three5Style {
+  public mainString = glsl`
+  float time = float0;
+  gl_FragColor = abs(vUV.x - sinN(time)) < 0.02 ? color0 : color1;
+  `
+  public uniforms = {
+    time: 0,
+    color0: new THREE.Vector4(),
+    color1: new THREE.Vector4()
   }
 }
 
 
+
+
 const testCalls = () => {
   const t5 = new Three5(10, 10)
+  const ls = new LineStyle()
   let i = 0
   //set the material for the circles
+  //three5.useStyle(ls)
   while (i++ < 10) {
     //update the material uniforms in a typesafe way
+    ls.uniforms.color0.setX(10)
     t5.circle(10, 10, 10, 10)
   }
 
