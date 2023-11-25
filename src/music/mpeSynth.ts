a8d73662eb66b81c58f2dc309a47706040186186
Avneesh Sarwate <avneeshsarwate@gmail.com>
2023-11-24 19:10:15 -0500

    more work on MPE api

--- src/music/mpeSynth.ts
+++ src/music/mpeSynth.ts
@@ -1,54 +1,60 @@
 import * as Tone from 'tone'
 
+type Constructor<T> = new (...args: any[]) => T
+
+interface VoiceGraph {
+  noteOn(note: number, velocity: number, pressure: number, slide: number): void
+  noteOff(): void
+  voiceFinishedCB?: (...args: any[]) => void
+  forceFinish(): void
+}
+
+interface MPEVoiceGraph extends VoiceGraph {
+  pitch: number
+  pressure: number
+  slide: number
+}
+
 class MPEPolySynth<T extends MPEVoiceGraph> {
   private vGraphCtor: Constructor<T>
   private maxVoices: number
-  private voices: T[]
+  private voices: Map<number, T> //map of voices by creation time via Date.now()
   private voiceIndex: number
 
   constructor(vGraph: Constructor<T>, maxVoices: number = 32) {
     this.vGraphCtor = vGraph
-
     this.maxVoices = maxVoices
-    this.voices = []
+    this.voices = new Map
     this.voiceIndex = 0
   }
 
   public noteOn(note: number, velocity: number, pressure: number, slide: number): T {
     let voice: T
 
-    if (this.voices.length < this.maxVoices) {
+    if (this.voices.size < this.maxVoices) {
       voice = new this.vGraphCtor()
-      this.voices.push(voice)
+      voice.noteOn(note, velocity, pressure, slide)
+      this.voices.set(Date.now(), voice)
     } else {
-      voice = this.voices[this.voiceIndex]
+      const minKey = Array.from(this.voices.keys()).sort()[0]
+      voice = this.voices.get(minKey)!
+      voice.forceFinish()
       voice.noteOn(note, velocity, pressure, slide)
     }
 
-    this.voiceIndex = (this.voiceIndex + 1) % this.maxVoices
     return voice
   }
 
-  // Additional methods like noteOff can be added here
+  public noteOff(voice: T): void {
+    this.voices.forEach((v, k) => {
+      if (v === voice) {
+        v.voiceFinishedCB = () => {
+          this.voices.delete(k)
+        }
+        v.noteOff()
+      }
+    })
+  }
 }
 
-interface VoiceGraph {
-  noteOn(note: number, velocity: number, pressure: number, slide: number): void
-  noteOff(): void
-}
 
-interface MPEVoiceGraph extends VoiceGraph {
-  //setters and getters for the numbers pitch, pressure, and slide
-  pitch: number
-  pressure: number
-  slide: number
-}
-
-type Constructor<T> = new (...args: any[]) => T
-
-// Example usage:
-// let polySynth = new MPEPolySynth("sine", (osc, pressure) => { /* ... */ }, (osc, slide) => { /* ... */ });
-// let voice = polySynth.noteOn("C4");
-// voice.changePitch("D4");
-// voice.changePressure(0.5);
-// voice.changeSlide(0.5);
