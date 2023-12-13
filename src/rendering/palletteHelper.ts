c3d8962a8bb8d76193bf8441c60622f4d77cb423
Avneesh Sarwate <avneeshsarwate@gmail.com>
2023-12-13 13:07:28 -0500

    adding color choice type

--- src/rendering/palletteHelper.ts
+++ src/rendering/palletteHelper.ts
@@ -51,7 +51,7 @@ function setUpComplementsUI(datGui: dat.GUI, colors: colorMap, ind: number) {
   //add reset all vars?
 }
 
-export function setUpColorDatGui(colorObj?: colorMap) {
+export function setUpColorDatGui(colorObj?: colorMap) : { datGui: dat.GUI, colors: colorChoices } {
   const datGui = new dat.GUI()
   const defaultColors = {
     color0: tinycolor.random().toHsv(),
@@ -71,6 +71,8 @@ export function setUpColorDatGui(colorObj?: colorMap) {
     setUpComplementsUI(datGui, colors, i)
   })
   console.log("COLORS", colors)
+
+  //@ts-ignore
   return { datGui, colors }
 }
 
@@ -90,4 +92,124 @@ export function palette(col1: tinycolor.ColorFormats.HSVA, col2: tinycolor.Color
   const col2Str = `rgb(${col2Rgb.r}, ${col2Rgb.g}, ${col2Rgb.b})`
   const palette = spectral.palette(col1Str, col2Str, count, spectral.RGBA)
   return palette.map((col) => tinycolor(col))
+}
+
+
+
+type colorChoices = {
+  color0: tinycolor.ColorFormats.HSVA,
+  color1: tinycolor.ColorFormats.HSVA,
+  color2: tinycolor.ColorFormats.HSVA,
+  color3: tinycolor.ColorFormats.HSVA,
+  color4: tinycolor.ColorFormats.HSVA,
+  col0tri0: tinycolor.ColorFormats.HSVA,
+  col0tri1: tinycolor.ColorFormats.HSVA,
+  col0tri2: tinycolor.ColorFormats.HSVA,
+  col0tet0: tinycolor.ColorFormats.HSVA,
+  col0tet1: tinycolor.ColorFormats.HSVA,
+  col0tet2: tinycolor.ColorFormats.HSVA,
+  col0tet3: tinycolor.ColorFormats.HSVA,
+  col0mon0: tinycolor.ColorFormats.HSVA,
+  col0mon1: tinycolor.ColorFormats.HSVA,
+  col0mon2: tinycolor.ColorFormats.HSVA,
+  col0mon3: tinycolor.ColorFormats.HSVA,
+  col0mon4: tinycolor.ColorFormats.HSVA,
+  col0mon5: tinycolor.ColorFormats.HSVA,
+  col0ana0: tinycolor.ColorFormats.HSVA,
+  col0ana1: tinycolor.ColorFormats.HSVA,
+  col0ana2: tinycolor.ColorFormats.HSVA,
+  col0ana3: tinycolor.ColorFormats.HSVA,
+  col0ana4: tinycolor.ColorFormats.HSVA,
+  col0ana5: tinycolor.ColorFormats.HSVA,
+  col0spl0: tinycolor.ColorFormats.HSVA,
+  col0spl1: tinycolor.ColorFormats.HSVA,
+  col0spl2: tinycolor.ColorFormats.HSVA,
+  col1tri0: tinycolor.ColorFormats.HSVA,
+  col1tri1: tinycolor.ColorFormats.HSVA,
+  col1tri2: tinycolor.ColorFormats.HSVA,
+  col1tet0: tinycolor.ColorFormats.HSVA,
+  col1tet1: tinycolor.ColorFormats.HSVA,
+  col1tet2: tinycolor.ColorFormats.HSVA,
+  col1tet3: tinycolor.ColorFormats.HSVA,
+  col1mon0: tinycolor.ColorFormats.HSVA,
+  col1mon1: tinycolor.ColorFormats.HSVA,
+  col1mon2: tinycolor.ColorFormats.HSVA,
+  col1mon3: tinycolor.ColorFormats.HSVA,
+  col1mon4: tinycolor.ColorFormats.HSVA,
+  col1mon5: tinycolor.ColorFormats.HSVA,
+  col1ana0: tinycolor.ColorFormats.HSVA,
+  col1ana1: tinycolor.ColorFormats.HSVA,
+  col1ana2: tinycolor.ColorFormats.HSVA,
+  col1ana3: tinycolor.ColorFormats.HSVA,
+  col1ana4: tinycolor.ColorFormats.HSVA,
+  col1ana5: tinycolor.ColorFormats.HSVA,
+  col1spl0: tinycolor.ColorFormats.HSVA,
+  col1spl1: tinycolor.ColorFormats.HSVA,
+  col1spl2: tinycolor.ColorFormats.HSVA,
+  col2tri0: tinycolor.ColorFormats.HSVA,
+  col2tri1: tinycolor.ColorFormats.HSVA,
+  col2tri2: tinycolor.ColorFormats.HSVA,
+  col2tet0: tinycolor.ColorFormats.HSVA,
+  col2tet1: tinycolor.ColorFormats.HSVA,
+  col2tet2: tinycolor.ColorFormats.HSVA,
+  col2tet3: tinycolor.ColorFormats.HSVA,
+  col2mon0: tinycolor.ColorFormats.HSVA,
+  col2mon1: tinycolor.ColorFormats.HSVA,
+  col2mon2: tinycolor.ColorFormats.HSVA,
+  col2mon3: tinycolor.ColorFormats.HSVA,
+  col2mon4: tinycolor.ColorFormats.HSVA,
+  col2mon5: tinycolor.ColorFormats.HSVA,
+  col2ana0: tinycolor.ColorFormats.HSVA,
+  col2ana1: tinycolor.ColorFormats.HSVA,
+  col2ana2: tinycolor.ColorFormats.HSVA,
+  col2ana3: tinycolor.ColorFormats.HSVA,
+  col2ana4: tinycolor.ColorFormats.HSVA,
+  col2ana5: tinycolor.ColorFormats.HSVA,
+  col2spl0: tinycolor.ColorFormats.HSVA,
+  col2spl1: tinycolor.ColorFormats.HSVA,
+  col2spl2: tinycolor.ColorFormats.HSVA,
+  col3tri0: tinycolor.ColorFormats.HSVA,
+  col3tri1: tinycolor.ColorFormats.HSVA,
+  col3tri2: tinycolor.ColorFormats.HSVA,
+  col3tet0: tinycolor.ColorFormats.HSVA,
+  col3tet1: tinycolor.ColorFormats.HSVA,
+  col3tet2: tinycolor.ColorFormats.HSVA,
+  col3tet3: tinycolor.ColorFormats.HSVA,
+  col3mon0: tinycolor.ColorFormats.HSVA,
+  col3mon1: tinycolor.ColorFormats.HSVA,
+  col3mon2: tinycolor.ColorFormats.HSVA,
+  col3mon3: tinycolor.ColorFormats.HSVA,
+  col3mon4: tinycolor.ColorFormats.HSVA,
+  col3mon5: tinycolor.ColorFormats.HSVA,
+  col3ana0: tinycolor.ColorFormats.HSVA,
+  col3ana1: tinycolor.ColorFormats.HSVA,
+  col3ana2: tinycolor.ColorFormats.HSVA,
+  col3ana3: tinycolor.ColorFormats.HSVA,
+  col3ana4: tinycolor.ColorFormats.HSVA,
+  col3ana5: tinycolor.ColorFormats.HSVA,
+  col3spl0: tinycolor.ColorFormats.HSVA,
+  col3spl1: tinycolor.ColorFormats.HSVA,
+  col3spl2: tinycolor.ColorFormats.HSVA,
+  col4tri0: tinycolor.ColorFormats.HSVA,
+  col4tri1: tinycolor.ColorFormats.HSVA,
+  col4tri2: tinycolor.ColorFormats.HSVA,
+  col4tet0: tinycolor.ColorFormats.HSVA,
+  col4tet1: tinycolor.ColorFormats.HSVA,
+  col4tet2: tinycolor.ColorFormats.HSVA,
+  col4tet3: tinycolor.ColorFormats.HSVA,
+  col4mon0: tinycolor.ColorFormats.HSVA,
+  col4mon1: tinycolor.ColorFormats.HSVA,
+  col4mon2: tinycolor.ColorFormats.HSVA,
+  col4mon3: tinycolor.ColorFormats.HSVA,
+  col4mon4: tinycolor.ColorFormats.HSVA,
+  col4mon5: tinycolor.ColorFormats.HSVA,
+  col4ana0: tinycolor.ColorFormats.HSVA,
+  col4ana1: tinycolor.ColorFormats.HSVA,
+  col4ana2: tinycolor.ColorFormats.HSVA,
+  col4ana3: tinycolor.ColorFormats.HSVA,
+  col4ana4: tinycolor.ColorFormats.HSVA,
+  col4ana5: tinycolor.ColorFormats.HSVA,
+  col4spl0: tinycolor.ColorFormats.HSVA,
+  col4spl1: tinycolor.ColorFormats.HSVA,
+  col4spl2: tinycolor.ColorFormats.HSVA
 }
