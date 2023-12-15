858f00011c20815fe2264f5609ed5af9de4aeb0c
Avneesh Sarwate <avneeshsarwate@gmail.com>
2023-12-15 00:16:26 -0500

    adding way to get handles for clips from node js script

--- abletonComms.ts
+++ abletonComms.ts
@@ -7,6 +7,29 @@ const test = async () => {
   // Establishes a connection with Live
   await ableton.start();
 
+  ableton.song.addListener("tracks", (tracks) => {
+    console.log("Tracks:", tracks);
+    tracks.forEach((track) => {
+      track.addListener("clip_slots", (clipSlots) => {
+        console.log("Clip slots:", clipSlots);
+        clipSlots.forEach((clipSlot) => {
+          clipSlot.addListener("has_clip", (hasClip) => {
+            console.log("Has clip:", hasClip);
+            if (hasClip) {
+              clipSlot.get("clip").then((clip) => {
+                console.log("Clip:", clip);
+                clip?.addListener("name", (name) => {
+                  console.log("Name:", name);
+                })
+              })
+            }
+          });
+        });
+      })
+    })
+  })
+    
+
   // Observe the current playback state and tempo
   ableton.song.addListener("is_playing", (p) => console.log("Playing:", p));
   ableton.song.addListener("tempo", (t) => console.log("Tempo:", t));
