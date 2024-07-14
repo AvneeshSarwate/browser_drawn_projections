418172956f1626cda7eb156afa6ebc38e25f9831
Avneesh Sarwate <avneesh@castle.xyz>
2024-07-14 09:40:23 -0400

    small version updates

--- src/music/mpeSynth.ts
+++ src/music/mpeSynth.ts
@@ -124,7 +124,7 @@ export class FatOscillatorVoice implements MPEVoiceGraph {
     // Tone.connect(this.outputGain, rawGain)
     // rawGain can also be at the end of the chain() call
 
-    this.oscillator.chain(this.filter, this.distortion, this.envelope, this.outputGain, Tone.Destination)
+    this.oscillator.chain(this.filter, this.distortion, this.envelope, this.outputGain, Tone.getDestination())
 
     this._pitch = 0
     this._pressure = 0
