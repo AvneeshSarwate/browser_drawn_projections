1bbdb694b7b35c983535a478b50a6491b9a063b5
Avneesh Sarwate <avneesh@castle.xyz>
2024-09-04 12:42:10 -1000

    refactoring stuff

--- src/channels/exports.ts
+++ src/channels/exports.ts
@@ -1,6 +1,7 @@
 //todo monaco livecoding - generate these files from the type-definition build script
 
-import { CancelablePromisePoxy, EventChop, Ramp, TimeContext, cos, cosN, launch, now, saw, sin, sinN, steps, tri, xyZip } from "./channels"
+import { EventChop, Ramp, cos, cosN, launch, now, saw, sin, sinN, steps, tri, xyZip } from "./channels"
+import { CancelablePromisePoxy, TimeContext } from "./base_time_context"
 
 export const channelExports = {
   CancelablePromisePoxy,
