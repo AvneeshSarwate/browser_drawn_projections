cbe34177d06674e8466242666a0caf55bbb721c1
Avneesh Sarwate <avneesh@castle.xyz>
2024-09-14 13:45:15 -0400

    fleshing out descriptions and styling around particular sketches

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -41,29 +41,36 @@ const nonSpecialSketches = sketchNames.filter(name => !specialSketchNames.includ
       <div class="sub-section-title">Sketches of note</div>
       <div>
         <router-link :to="`/clickAVMelodyLauncher`">An interactive melody player</router-link>: A simple example 
-        that unifies musical sequencing and timing of visual events with the same codebase. One of the core problems
-        we are trying to address with our tool is the synchronization of musical and visual events with simple, intuitive tools. <a href="https://github.com/AvneeshSarwate/browser_drawn_projections/blob/main/src/sketches/clickAVMelodyLauncher/LivecodeHolder.vue">Source</a>
+        that unifies musical sequencing and timing of visual events, and GPU driven post-processing in the same codebase. One of the core problems
+        we are trying to address with our tool is the synchronization of musical and visual events with simple, intuitive tools. <a href="https://github.com/AvneeshSarwate/browser_drawn_projections/blob/main/src/sketches/clickAVMelodyLauncher/LivecodeHolder.vue">Source</a>. 
+        This sketch also demonstrates our TouchDesigner-inspired API for shader-based post-processing (<a href="https://github.com/AvneeshSarwate/browser_drawn_projections/blob/main/src/sketches/clickAVMelodyLauncher/LivecodeHolder.vue#L133-L145">usage</a>, 
+        <a href="https://github.com/AvneeshSarwate/browser_drawn_projections/blob/main/src/rendering/customFX.ts#L76">implementation entry point</a>).
       </div>
+      <br>
       <div>
         <router-link :to="`/tonePianoSequencer`">An experimental sequencer</router-link>: Another example that coordinates musical sequencing,
         UI interaction, and animation all with the same codebase. <a href="https://github.com/AvneeshSarwate/browser_drawn_projections/blob/main/src/sketches/tonePianoSequencer/LivecodeHolder.vue">Source</a>
       </div>
+      <br>
       <div>
         <router-link :to="`/three5Example`">p5.js + three.js</router-link>: Another goal of ours is to simplify the workflow for creating GPU driven
         animations that don't fall into the traditional 3D realm. This example shows our initial attempts at building a p5.js style API
         on top of three.js to enable a "power 2D" workflow  - eg, creating an API for simple 2D geometry drawing that also allows for easily integrating
         custom shaders and materials. The source can be found <a href="https://github.com/AvneeshSarwate/browser_drawn_projections/blob/main/src/rendering/three5.ts">here</a> and usage 
-        can be seen in the <a href="https://github.com/AvneeshSarwate/browser_drawn_projections/blob/main/src/sketches/three5Example/LivecodeHolder.vue">index.ts</a> file.
+        can be seen <a href="https://github.com/AvneeshSarwate/browser_drawn_projections/blob/main/src/sketches/three5Example/LivecodeHolder.vue">here</a>.
       </div>
       <div class="sub-section-title">Other sketches</div>
-      <div>
-        These sketches are proofs of concept of various engineering and API design challenges.
-      </div>
-      <div id="sketchList">
-        <router-link v-for="sketchName in sketchNames" :key="sketchName" :to="`/${sketchName}`">
-          <div> {{ sketchName }} </div>
-        </router-link>
-      </div>
+      <details>
+        <summary>
+          These sketches are proofs of concept of various engineering and API design challenges (click to expand).
+        </summary>
+        
+        <div id="sketchList">
+          <router-link v-for="sketchName in sketchNames" :key="sketchName" :to="`/${sketchName}`">
+            <div> {{ sketchName }} </div>
+          </router-link>
+        </div>
+      </details>
     </div>
   </div>
 </template>
@@ -100,7 +107,7 @@ const nonSpecialSketches = sketchNames.filter(name => !specialSketchNames.includ
   font-size: 16px;
 }
 
-.intro > div > a, #sketchList > a {
+.intro > div > a, #sketchList > a, .sketchlist > div > a {
   color: #333;
   text-decoration:underline;
   text-underline-offset: 8px;
@@ -108,7 +115,7 @@ const nonSpecialSketches = sketchNames.filter(name => !specialSketchNames.includ
   text-decoration-color: #aaa;
 }
 
-.intro > div > a:hover, #sketchList > a:hover {
+.intro > div > a:hover, #sketchList > a:hover, .sketchlist > div > a:hover {
   color: #333;
   text-decoration:underline;
   text-underline-offset: 8px;
