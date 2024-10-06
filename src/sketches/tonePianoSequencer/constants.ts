379d619029ae44e11b16fcc1c3b51d068fb2a227
Sumanth Srinivasan <sumanth.srinivasan@gmail.com>
2024-10-06 12:57:28 -0400

    Customize colors

---
+++ src/sketches/tonePianoSequencer/constants.ts
@@ -0,0 +1,16 @@
+export const subwayColors: string[] = [
+    '#0039A6', 
+    '#FF6319', 
+    '#6CBE45', 
+    '#996633', 
+    '#A7A9AC', 
+    '#FCCC0A', 
+    '#808183', 
+    '#EE352E', 
+    '#00933C', 
+    '#B933AD',
+]
+
+export const cMajNoteStrings = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5', 'C6']
+
+export const dLydianNoteStrings = ['D4', 'E4', 'F#4', 'G#4', 'A4', 'B4', 'C#5', 'D5', 'E5', 'F#5', 'G#5', 'A5', 'B5', 'C#6']
