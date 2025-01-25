db553badc93bd5ba1bc6caff3aae9359c19afce3
Avneesh Sarwate <avneesh@castle.xyz>
2025-01-25 17:43:08 -0500

    removing react vite dependencies and downgrading vite to v5 to build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import vueJsx from '@vitejs/plugin-vue-jsx'
-import react from '@vitejs/plugin-react';
+// import react from '@vitejs/plugin-react';
 import ts from 'typescript';
 import { vitePluginTypescriptTransform } from 'vite-plugin-typescript-transform';
 
@@ -12,7 +12,7 @@ export default defineConfig({
   base: "/",
   plugins: [
     vue(), 
-    react(),
+    // react(),
     // vueJsx()
     vitePluginTypescriptTransform({
       enforce: 'pre',
