90b4a359d93f1845b22a59af96fe8006c7c61ec4
Avneesh Sarwate <avneesh@castle.xyz>
2025-01-26 20:44:45 -0500

    draft of melody process and notes for simple melody generation algorithm

--- src/sketches/mutek_playground/audiovisualProcesses.ts
+++ src/sketches/mutek_playground/audiovisualProcesses.ts
@@ -1,4 +1,4 @@
-import type { BrowserTimeContext } from "@/channels/channels";
+import { biasedTri, type BrowserTimeContext } from "@/channels/channels";
 import type { Dancer } from "./dancerInitializer";
 import type { MPEPolySynth, MPEVoiceGraph } from "@/music/mpeSynth";
 import type { AbletonClip, AbletonNote } from "@/io/abletonClips";
@@ -52,21 +52,49 @@ const playNote = <T extends MPEVoiceGraph>(ctx: BrowserTimeContext, noteData: Ab
   })
 }
 
+
+const arrayRandChoice = <T>(arr: T[]) => {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
 const dancePhrase = <T extends MPEVoiceGraph>(dancer: Dancer, clip: AbletonClip, synth: MPEPolySynth<T>, ctx: BrowserTimeContext) => {
   const phraseDuration = clip.duration
+  const startColors = {
+    color1: dancer.dancerShapeUniforms.color1.value.clone(),
+    color2: dancer.dancerShapeUniforms.color2.value.clone(),
+    color3: dancer.dancerShapeUniforms.color3.value.clone(),
+    color4: dancer.dancerShapeUniforms.color4.value.clone(),
+    color5: dancer.dancerShapeUniforms.color5.value.clone(),
+    color6: dancer.dancerShapeUniforms.color6.value.clone(),
+  }
+  const colorKeys = Object.keys(startColors)
   ctx.branch(async ctx => {
     for (const [i, note] of clip.noteBuffer().entries()) {
       await ctx.wait(note.preDelta)
       playNote(ctx, note.note, synth)
+
+      ctx.branch(async ctx => {
+        const duration = 0.3
+        const startTime = ctx.time
+        while(ctx.time - startTime < duration) {
+          const rampVal = biasedTri((ctx.time - startTime) / duration, 0.25)
+          const colorKey = arrayRandChoice(colorKeys)
+          const newColor = startColors[colorKey].clone().multiplyScalar(rampVal)
+          dancer.dancerShapeUniforms[colorKey].value = newColor
+          await ctx.waitSec(0.016)
+        }
+      })
+
       await ctx.wait(note.postDelta ?? 0)
     }
   })
-  ctx.branch(async ctx => {
-    const startTime = ctx.time
-    while(ctx.time - startTime < phraseDuration) {
-      const frame = Math.floor((ctx.time - startTime) / phraseDuration * dancer.params.frameCount)
-      dancer.setFrame(frame)
-      await ctx.waitSec(0.016)
-    }
-  })
 }
+
+/*
+music algorithm
+pick root note - either same as last, or move some random amount
+pick melodic contour - either same as last, 1 note diff, new (only if cache not full), or from cache
+  melodic contour is deltas from root note - 2-6 notes, all same duration
+  when fully new melodic contour is made, cache it if cache size < 5
+wait 2-6 8th notes before playing next phrase (or with some small prob, no extra wait)
+*/
